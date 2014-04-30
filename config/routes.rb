49ad86a2a33ff6b340f8aefc5d680d028d64a554
taylorchiu <taylorparkerchiu@gmail.com>
2014-04-29 23:22:31 -0700

    Phase 2 and 3 views working but not redirecting to correct page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,45 @@
 Blog::Application.routes.draw do
+  resources :users, :posts, :comments
+
   root "posts#index"
 
-  resources :posts
-  resources :comments
+  get '/signup', to: 'users#new'
+  delete '/signout', to: 'sessions#destroy'
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+
 end
+
+
+#       Prefix Verb   URI Pattern                  Controller#Action
+#        users GET    /users(.:format)             users#index
+#              POST   /users(.:format)             users#create
+#     new_user GET    /users/new(.:format)         users#new
+#    edit_user GET    /users/:id/edit(.:format)    users#edit
+#         user GET    /users/:id(.:format)         users#show
+#              PATCH  /users/:id(.:format)         users#update
+#              PUT    /users/:id(.:format)         users#update
+#              DELETE /users/:id(.:format)         users#destroy
+#        posts GET    /posts(.:format)             posts#index
+#              POST   /posts(.:format)             posts#create
+#     new_post GET    /posts/new(.:format)         posts#new
+#    edit_post GET    /posts/:id/edit(.:format)    posts#edit
+#         post GET    /posts/:id(.:format)         posts#show
+#              PATCH  /posts/:id(.:format)         posts#update
+#              PUT    /posts/:id(.:format)         posts#update
+#              DELETE /posts/:id(.:format)         posts#destroy
+#     comments GET    /comments(.:format)          comments#index
+#              POST   /comments(.:format)          comments#create
+#  new_comment GET    /comments/new(.:format)      comments#new
+# edit_comment GET    /comments/:id/edit(.:format) comments#edit
+#      comment GET    /comments/:id(.:format)      comments#show
+#              PATCH  /comments/:id(.:format)      comments#update
+#              PUT    /comments/:id(.:format)      comments#update
+#              DELETE /comments/:id(.:format)      comments#destroy
+#         root GET    /                            posts#index
+#       signup GET    /signup(.:format)            users#new
+#      signout DELETE /signout(.:format)           sessions#destroy
+#       signin GET    /signin(.:format)            sessions#new
+#              POST   /signin(.:format)            sessions#create
+
+
